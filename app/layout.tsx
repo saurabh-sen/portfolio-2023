37df96d94732c426a2a2cedef754e430565d9578
Saurabh Sen <saurabh.sen.cs19@ggits.net>
2023-07-29 08:30:42 +0530

    FIX: removed exports

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import * as React from 'react';
-import { Bungee_Outline } from 'next/font/google'
 import Providers from './Providers'
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: 'Saurabh Sen - Portfolio',
   description: 'I am a frontend developer, I have experience in building web applications using React, Next.js, Tailwind, and other modern web technologies.',
 }
-
-export const BungeeOutline = Bungee_Outline({
-  subsets: ['latin'],
-  weight: '400'
-})
 
 export default function RootLayout({
   children,
